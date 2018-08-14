2c814606339bfb2b6a9451fa578107bb0be39946
aircraftman3 <aircraftman3@gmail.com>
2018-08-14 22:21:54 +0200

    more js. Playlist styling.

--- script.js
+++ script.js
@@ -25,31 +25,31 @@ $(document).ready(function(){
 var song1 = {
   artist: "Icona Pop",
   title: "Still Don't Know",
-  duration: "3:16"
+  duration: "3:16\xa0\xa0|\xa0\xa0"
 }
 
 var song2 = {
   artist: song1.artist,
   title: "I Love It",
-  duration: "2:35"
+  duration: "2:35\xa0\xa0|\xa0\xa0"
 }
 
 var song3 = {
   artist: song1.artist,
   title: "Girlfriend",
-  duration: "2:50"
+  duration: "2:50\xa0\xa0|\xa0\xa0"
 }
 
 var song4 = {
   artist: song1.artist,
   title: "We Got the World",
-  duration: "3:07"
+  duration: "3:07\xa0\xa0|\xa0\xa0"
 }
 
 var song5 = {
   artist: song1.artist,
   title: "Nights Like This",
-  duration: "3:24"
+  duration: "3:24\xa0\xa0|\xa0\xa0"
 }
 
 document.getElementById("artist").innerHTML = song1.artist;
@@ -58,7 +58,7 @@ document.getElementById("song-title").innerHTML = song1.name;
 
 
 
-function tryMe(object, id) {
+function createPlaylist(object, id) {
   var para = document.createElement("p");
   var node = document.createTextNode(object);
   para.appendChild(node);
@@ -66,6 +66,43 @@ function tryMe(object, id) {
   element.appendChild(para);
 }
 
-tryMe(song1.duration, "song1");
-tryMe(song1.artist, "song1");
-tryMe(song1.title, "song1");
+function createPlaylist2(id) {
+  var img = document.createElement("img");
+  img.src = "./images/btn-share-small.png";
+  img.className = "first-image";
+  document.getElementById(id).appendChild(img);
+}
+
+function createPlaylist3(id) {
+  var img = document.createElement("img");
+  img.src = "./images/btn-heart-small.png";
+  img.className = "second-image";
+  document.getElementById(id).appendChild(img);
+}
+
+
+createPlaylist(song1.duration, "song1");
+createPlaylist(song1.artist, "song1");
+createPlaylist(song1.title, "song1.1");
+createPlaylist2("song1")
+createPlaylist3("song1")
+createPlaylist(song2.duration, "song2");
+createPlaylist(song2.artist, "song2");
+createPlaylist(song2.title, "song2.1");
+createPlaylist2("song2")
+createPlaylist3("song2")
+createPlaylist(song3.duration, "song3");
+createPlaylist(song3.artist, "song3");
+createPlaylist(song3.title, "song3.1");
+createPlaylist2("song3")
+createPlaylist3("song3")
+createPlaylist(song4.duration, "song4");
+createPlaylist(song4.artist, "song4");
+createPlaylist(song4.title, "song4.1");
+createPlaylist2("song4")
+createPlaylist3("song4")
+createPlaylist(song5.duration, "song5");
+createPlaylist(song5.artist, "song5");
+createPlaylist(song5.title, "song5.1");
+createPlaylist2("song5")
+createPlaylist3("song5")
