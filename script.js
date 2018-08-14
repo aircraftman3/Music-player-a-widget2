c5ae83cdb5bac341f34ef785b1a7dd09434c12e5
aircraftman3 <aircraftman3@gmail.com>
2018-08-14 19:44:15 +0200

    more js. Playlist styling.

--- script.js
+++ script.js
@@ -21,3 +21,51 @@ $(document).ready(function(){
     $(".player").fadeIn();
   });
 });
+
+var song1 = {
+  artist: "Icona Pop",
+  title: "Still Don't Know",
+  duration: "3:16"
+}
+
+var song2 = {
+  artist: song1.artist,
+  title: "I Love It",
+  duration: "2:35"
+}
+
+var song3 = {
+  artist: song1.artist,
+  title: "Girlfriend",
+  duration: "2:50"
+}
+
+var song4 = {
+  artist: song1.artist,
+  title: "We Got the World",
+  duration: "3:07"
+}
+
+var song5 = {
+  artist: song1.artist,
+  title: "Nights Like This",
+  duration: "3:24"
+}
+
+document.getElementById("artist").innerHTML = song1.artist;
+document.getElementById("song-title").innerHTML = song1.name;
+
+
+
+
+function tryMe(object, id) {
+  var para = document.createElement("p");
+  var node = document.createTextNode(object);
+  para.appendChild(node);
+  var element = document.getElementById(id);
+  element.appendChild(para);
+}
+
+tryMe(song1.duration, "song1");
+tryMe(song1.artist, "song1");
+tryMe(song1.title, "song1");
